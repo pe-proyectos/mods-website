8e6797223c4fbcd96d9db88e1ccaf4c08ced248d
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-08-15 10:25:32 -0500

    feat: responsive header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,35 +5,53 @@ import { FaDiscord } from 'react-icons/fa';
 import lenguagues from '../constants/languages.json';
 const Header = () => {
   return (
-    <nav className="bg-primary sticky top-0 z-50 w-full flex flex-row items-center h-[9vh] max-h-[9vh]  px-6 py-2 justify-between">
-      <a className="cursor-pointer object-cover w-1/4 max-lg:w-1/3" href="/">
-        <img src="/images/logo-large.png" alt="logo" className="hover:opacity-80" />
+    <nav className="bg-primary sticky top-0 z-50 w-full flex flex-row items-center h-[9vh] max-h-[9vh]  px-6 py-2 justify-between max-sm:px-3 max-sm:max-h-[7vh] ">
+      <a
+        className="cursor-pointer object-cover w-[25%] min-w-[280px] max-md:min-w-min  "
+        href="/"
+      >
+        <img
+          src="/images/logo-large.png"
+          alt="logo large"
+          className="hover:opacity-80 max-md:hidden"
+        />
+        <img
+          src="/images/logo-sm.png"
+          alt="logo medium"
+          className="hover:opacity-80 hidden max-md:block max-sm:hidden w-[130px] "
+        />
+        <img
+          src="/images/logo.png"
+          alt="logo small"
+          className="hover:opacity-80 hidden max-sm:block w-[70px]"
+        />
       </a>
-      <div className=" text-text_primary text-[14px]">
+      <div className=" text-text_primary text-[14px] max-lg:hidden">
         Inicia sesión para deshabilitar los anuncios
       </div>
       <div className="flex flex-row items-center  text-text_primary font-semibold text-[14px] h-full">
         <a
-          className="cursor-pointer hover:bg-shadow px-5   h-full content-center rounded-md"
+          className="cursor-pointer hover:bg-shadow px-5 max-sm:px-2.5  h-full content-center rounded-md flex flex-row gap-2 items-center"
           href="/"
         >
-          Red Loader
+          <img src="/images/redlogo.png " className="w-14 hidden max-sm:block" />
+          <p className="max-sm:hidden"> Red Loader</p>
         </a>
 
         <a
-          className="cursor-pointer hover:bg-shadow px-5 h-full content-center rounded-md flex flex-row gap-1 items-center"
+          className="cursor-pointer hover:bg-shadow px-5 max-sm:px-2.5 h-full content-center rounded-md flex flex-row gap-1 items-center"
           href="https://discord.com/invite/sotf"
           target="_blank"
         >
           <FaDiscord className="size-6 text-text_primary" />
-          Discord
+          <p className="max-sm:hidden">Discord</p>
         </a>
 
         <div className="dropdown dropdown-end  h-full content-center ">
           <div tabIndex={0} role="button" className="h-full flex items-center ">
-            <a className="cursor-pointer hover:bg-shadow px-5 h-full content-center rounded-md flex flex-row gap-1 items-center">
-              <AiOutlineGlobal className="size-6 text-text_primary" />
-              ES
+            <a className="cursor-pointer hover:bg-shadow px-5 max-sm:px-2.5  h-full content-center rounded-md flex flex-row gap-1 items-center">
+              <AiOutlineGlobal className="size-6 text-text_primary " />
+              <p className="max-sm:hidden">ES</p>
             </a>
           </div>
           <ul
