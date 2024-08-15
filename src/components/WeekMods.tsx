f0a8bcd221b3cabbb1b05ce693d8893380189863
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-08-15 17:25:40 -0500

    feat:create-WeekMods

---
+++ src/components/WeekMods.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CardWeekMod } from './Cards';
+import dataWeekMod from '../constants/dataWeekMods.json';
+const WeekMods = () => {
+  return (
+    <section className="w-[90%] mx-auto flex flex-col gap-8 items-center">
+      <h2 className="text-text_primary text-4xl font-semibold">Mods de la semana</h2>
+      <div className="flex flex-wrap justify-around  min-h-64">
+        {dataWeekMod.map((item) => {
+          return (
+            <CardWeekMod
+              topNumber={item.topNumber}
+              abstract={item.abstract}
+              downloadNumbersWeek={item.downloadNumbersWeek}
+              downloadNumbersTotal={item.downloadNumbersTotal}
+              id={item.id}
+              img={item.img}
+              owner={item.owner}
+              title={item.title}
+              key={item.id}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default WeekMods;
