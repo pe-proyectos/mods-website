6e1380e024f3265325d6063c33f7e9cb13ccf8fa
JoseEnriqueMendo <dinofiesta123@gmail.com>
2024-08-16 16:12:34 -0500

    feat:create-RegisterView

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -18,7 +18,7 @@ export const FormLogin: React.FC<{}> = () => {
           <span className="label-text">Contraseña</span>
         </div>
         <input
-          type="text"
+          type="password"
           placeholder="Ingresa tu correo contraseña"
           className="input input-bordered max-w-xs bg-transparent rounded-full w-[300px] max-[500px]:w-full   "
         />
@@ -32,6 +32,58 @@ export const FormLogin: React.FC<{}> = () => {
       >
         {'!Regítrate Aquí'}
       </p>
+
+      <button className="btn btn-success bg-text_green hover:bg-[#18A048] w-[300px] rounded-full  max-[500px]:w-full  ">
+        Inciar Sesión
+      </button>
+    </form>
+  );
+};
+
+export const FormRegister: React.FC<{}> = () => {
+  return (
+    <form className="bg-secundary w-full  flex flex-col gap-4 p-8 items-center max-[900px]:p-6 max-[500px]:p-4 ">
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Correo electrónico</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Ingresa tu correo electrónico"
+          className="input input-bordered w-full max-w-xs bg-transparent rounded-full"
+        />
+      </label>
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Nombre de usuario</span>
+        </div>
+        <input
+          type="text"
+          placeholder="Ingresa tu correo nombre de usuario"
+          className="input input-bordered w-full max-w-xs bg-transparent rounded-full"
+        />
+      </label>
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Contraseña</span>
+        </div>
+        <input
+          type="password"
+          placeholder="Ingresa tu correo contraseña"
+          className="input input-bordered max-w-xs bg-transparent rounded-full w-[300px] max-[500px]:w-full   "
+        />
+      </label>
+
+      <label className="form-control w-full max-w-xs">
+        <div className="label">
+          <span className="label-text">Confirmar contraseña</span>
+        </div>
+        <input
+          type="password"
+          placeholder="Repite tu contraseñ"
+          className="input input-bordered max-w-xs bg-transparent rounded-full w-[300px] max-[500px]:w-full   "
+        />
+      </label>
 
       <button className="btn btn-success bg-text_green hover:bg-[#18A048] w-[300px] rounded-full  max-[500px]:w-full  ">
         Inciar Sesión
